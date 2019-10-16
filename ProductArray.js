53cadd7c15385d522f9059d6436d67a7cdefba70
Libby Hamlin <bravolibby@gmail.com>
2019-10-16 12:52:55 -0700

    updated productArray.js and results.js files

--- ProductArray.js
+++ ProductArray.js
@@ -3,6 +3,10 @@ export class ProductArray {
         this.products = products.slice();
     }
 
+    getProducts() {
+        return this.products;
+    }
+
     getRandomProduct() {
         const randomProductIndex = Math.floor(Math.random() * this.products.length);
 
